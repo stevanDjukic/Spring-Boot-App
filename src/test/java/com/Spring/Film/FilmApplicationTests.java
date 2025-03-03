809622989d9316daec8135a6426d70f501cf2b45
Stevan Djukic <stevan.djukic26@gmail.com>
2025-03-04 00:46:32 +0100

    Initial commit - project setup

---
+++ src/test/java/com/Spring/Film/FilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.Film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
